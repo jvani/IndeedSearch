a6fe641aa3bc368344bbb83b57473139904c818e
Jordan Vani <jmv423@nyu.edu>
2018-08-05 02:05:12 -0400

    Web interface; init.

---
+++ Crawl/app.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import logging
+from crawler_worker import *
+from indeed.spiders.indeed import *
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from flask_bootstrap import Bootstrap
+from flask import Flask, request, render_template, send_from_directory
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+class CrawlForm(Form):
+    query = TextField(
+        "Query:", 
+        validators=[validators.required()],
+        description="Data Scientist"
+    )
+    location = TextField(
+        "Location:", 
+        validators=[validators.required()],
+        description="Seattle"
+    )
+    domain = TextField(
+        "Domain:", 
+        validators=[validators.required()],
+        description=".com"
+    )
+    index = TextField(
+        "Elasticsearch Index:", 
+        validators=[validators.required()],
+        description="indeed"
+    )
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    """"""
+    running_spider = None
+    form = CrawlForm(request.form)
+    
+    if request.method == "POST":
+        try:
+
+            # -- Init spider with form data.
+            crawler = CrawlerWorker(IndeedSpider, request.form.to_dict())
+            crawler.start()
+        # -- Update running_spider var based on try success.
+        except:
+            running_spider = False
+        else:
+            running_spider = True
+
+    return render_template("home.html", form=form, running_spider=running_spider)
+
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ Crawl/crawler_worker.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from scrapy import signals
+from scrapy.crawler import CrawlerProcess
+from scrapy.xlib.pydispatch import dispatcher
+import multiprocessing
+
+
+class CrawlerWorker(multiprocessing.Process):
+
+    def __init__(self, spider, spider_kwargs):
+        multiprocessing.Process.__init__(self)
+        # -- Store objects.
+        self.spider = spider
+        self.spider_kwargs = spider_kwargs
+
+        self.process = CrawlerProcess()
+        self.items = []
+        dispatcher.connect(self._item_passed, signals.item_passed)
+
+
+    def _item_passed(self, item):
+        self.items.append(item)
+
+
+    def run(self):
+        self.process.crawl(self.spider, **self.spider_kwargs)
+        self.process.start()
